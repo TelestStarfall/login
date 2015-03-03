56f3c7d5435b7bea8ad220da8b146967d3c24484
Telest <valou.pannacotta@gmail.com>
2015-03-03 16:22:39 +0100

    Update options.js

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -3,8 +3,8 @@ function onDialogAccept()
 	var login = document.getElementById("login").value;
 	var passwd = document.getElementById("passwd").value;
 	
-	Services.console.logStringMessage(passwd);
-	Services.console.logStringMessage(login);
+	window.alert(passwd);
+	window.alert(login);
 	
 	Services.prefs.setIntPref("extensions.button_login.login", parseInt(login));
 	Services.prefs.setIntPref("extensions.button_login.password", parseInt(passwd));
