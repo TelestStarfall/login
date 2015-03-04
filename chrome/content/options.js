b3409b2d56e35ac52e2765330e193c08e3c1c8b9
Telest <valou.pannacotta@gmail.com>
2015-03-04 16:11:23 +0100

    Update options.js

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -2,37 +2,59 @@
 var lm = Components.classes["@mozilla.org/login-manager;1"].getService( 
 	Components.interfaces.nsILoginManager
 );
+var nsLoginInfo = new Components.Constructor(
+	"@mozilla.org/login-manager/loginInfo;1",
+	Components.interfaces.nsILoginInfo,
+	"init"
+);
+var hostname = 'http://myserv';
+var formSubmitURL = 'null';
+var httprealm = 'http://myserv';
 
 function onLoad(){
-	var username;
-	var password;
 	let passwd = document.getElementById("passwd");
 	let login = document.getElementById("login");
+	var username = "";
+	var password = "";
 	
 	try { 
 		//Recherche selon les critères
-		//var logins = lm.findLogins({}, hostname, formSubmitURL, httprealm);
-		var logins = lm.getAllLogins({});
+		var logins = lm.findLogins({}, hostname, formSubmitURL, httprealm);
+		//var logins = lm.getAllLogins({});
 		//Recherche en fonction du tableau renvoyé
 		for (var i = 0; i < logins.length; i++) {
-			//if (logins[i].username == username) {
+			if (logins[i].username) {
 				password = logins[i].password;
 				username = logins[i].username;
-				//break;
-			//}
+				break;
+			}
 		}
 	}
 	//Si il ne trouve rien
 	catch(ex) {
-		window.alert("This will only happen if there is no nsILoginManager component class");
+		password = "";
+		username = "";
 	}
+	
 	passwd.value = password;
 	login.value = username;
 }
 
 //A la validation du formulaire 
 function onDialogAccept(){
-	//Rien ne se passe, la fanêtre se ferme
+	var login = document.getElementById("login").value;
+ 	var passwd = document.getElementById("passwd").value;
+	
+	var extLoginInfo = new nsLoginInfo(
+		hostname,
+		formSubmitURL,
+		httprealm,
+		login,
+		passwd,
+		"",
+		""
+	);
+	lm.addLogin(extLoginInfo);
 }
 
 //En cliquant sur le bouton cancel
