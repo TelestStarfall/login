c575e101ea4a3b7877273269e042bb8c14d59e0f
Telest <valou.pannacotta@gmail.com>
2015-03-04 14:05:19 +0100

    Update overlay.js

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,16 +1,38 @@
 function startup() {
 	var button = document.getElementById("my-button");
-	button.addEventListener("click", showDate);
+	button.addEventListener("click", showUser);
 }
 
 window.addEventListener("load", function(e) { 
 	startup(); 
 }, false);
 
-function showDate(){
-	var date = new Date();
-	var day = date.getDay();
-	var dateString = date.getFullYear() + "." + (date.getMonth()+1) + "." + date.getDate();
+function showUser(){
+	var username;
+	var password;
 	
-	window.alert("Date : " + dateString);
+	try { 
+		//invocation du login manager
+		var lm = Components.classes["@mozilla.org/login-manager;1"].getService( 
+			Components.interfaces.nsILoginManager
+		);
+		
+		//Recherche selon les critères
+		//var logins = lm.findLogins({}, hostname, formSubmitURL, httprealm);
+		var logins = lm.getAllLogins({});
+		//Recherche en fonction du tableau renvoyé
+		for (var i = 0; i < logins.length; i++) {
+			//if (logins[i].username == username) {
+				password = logins[i].password;
+				username = logins[i].username;
+				//break;
+			//}
+		}
+	}
+	//Si il ne trouve rien
+	catch(ex) {
+		window.alert("This will only happen if there is no nsILoginManager component class");
+	}
+	
+	window.alert("Hello "+username+" !");
 }
