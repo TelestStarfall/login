d6218d4c3fc125f056b54dd091ef8c43774379e8
Telest <valou.pannacotta@gmail.com>
2015-03-04 13:58:52 +0100

    Update options.js

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -3,54 +3,36 @@ var lm = Components.classes["@mozilla.org/login-manager;1"].getService(
 	Components.interfaces.nsILoginManager
 );
 
-//Constructeur
-var nsLoginInfo = new Components.Constructor(
-	"@mozilla.org/login-manager/loginInfo;1",
-	Components.interfaces.nsILoginInfo,
-	"init"
-);  
-//Variables d'utilisation
-var hostname = 'chrome://firefoo';
-var formSubmitURL = 'null';
-var httprealm = 'User Registration';
-var username;
-var password;
-var login = document.getElementById("login").value;
-var passwd = document.getElementById("passwd").value;
-
-try { 
-	//Recherche selon les critères
-	var logins = lm.findLogins({}, hostname, formSubmitURL, httprealm);
+function onLoad(){
+	var username;
+	var password;
+	let passwd = document.getElementById("passwd").value;
+	let login = document.getElementById("login").value;
 	
-	//Recherche en fonction du tableau renvoyé
-	for (var i = 0; i < logins.length; i++) {
-		if (logins[i].username == username) {
-			password = logins[i].password;
-			break;
+	try { 
+		//Recherche selon les critères
+		//var logins = lm.findLogins({}, hostname, formSubmitURL, httprealm);
+		var logins = lm.getAllLogins({});
+		//Recherche en fonction du tableau renvoyé
+		for (var i = 0; i < logins.length; i++) {
+			//if (logins[i].username == username) {
+				password = logins[i].password;
+				username = logins[i].username;
+				//break;
+			//}
 		}
 	}
-	
-	passwd = password; 
-}
-//Si il ne trouve rien
-catch(ex) {
-	window.alert("This will only happen if there is no nsILoginManager component class");
+	//Si il ne trouve rien
+	catch(ex) {
+		window.alert("This will only happen if there is no nsILoginManager component class");
+	}
+	passwd.value = password;
+	login.value = username;
 }
 
 //A la validation du formulaire 
 function onDialogAccept(){
-	
-	
-	var extLoginInfo = new nsLoginInfo(
-	hostname,
-	formSubmitURL,
-	httprealm,
-	login,
-	passwd,
-	"",
-	""
-);
-	pm.addLogin(extLoginInfo);
+	//Rien ne se passe, la fanêtre se ferme
 }
 
 //En cliquant sur le bouton cancel
